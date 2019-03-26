89cd145ff1eea768db025e7b345ee94632cbec72
Jonas Hess <jonas.hess@revier.de>
2019-03-26 20:52:49 +0100

    +- sweeping changed completely

--- Controller/Admin/sweepTemp.php
+++ Controller/Admin/sweepTemp.php
@@ -1,63 +1,23 @@
-<?
-/*	OXID SweepTemp
-	Author: jonas.hess@revier.de
-	Licence: GPLv3
-*/
+<?php
 
-$tmp_folder = '../../../../../tmp';
-$tmp_garbage = Array();
-$tmp_smarty = Array();
+class sweepTemp extends sweepTemp_parent
+{
+    public function render()
+    {
+        return parent::render();
+    }
 
-if (!isset($_COOKIE['admin_sid'])) {
-    die('no admin session found');
-}
-
-//Define the Files to sweep by Regex
-$tmp_garbage[] = '^[0-9a-z\^\%A-Z_]*oxforgotpw[0-9a-z\^\%A-Z_]*\.php$';
-$tmp_garbage[] = '^[0-9a-z\^%A-Z_]*oxcontent[0-9]*oxbaseshop\.php$';
-$tmp_garbage[] = '^[0-9a-z\^\%A-Z_]*\.tpl\.php$';
-$tmp_garbage[] = '^[0-9a-z\^\%A-Z_]*sort[0-9a-z\^\%A-Z_]*\.tpl\.php$';
-$tmp_garbage[] = '^[0-9a-z\^\%A-Z_]*basket[0-9a-z\^\%A-Z_]*\.tpl\.php$';
-$tmp_garbage[] = '^[0-9a-z\^%A-Z_]*\_i18n\.txt$';
-$tmp_garbage[] = '^[0-9a-z\^%A-Z_]*\_allfields\_1\.txt$';
-$tmp_garbage[] = '^oxpec\_[0-9a-z\^%A-Z_]*seo\.txt$';
-$tmp_garbage[] = '^oxpec\_[0-9a-z\^%A-Z_]*allviews\.txt$';
-$tmp_garbage[] = '^oxpec\_[0-9a-z\^%A-Z_]*\_allfields\_\.txt$';
-$tmp_garbage[] = '^oxpec\_langcache\_[0-9a-z\^%A-Z_]*\_default\.txt$';
-$tmp_garbage[] = '^oxpec\_[0-9a-z\^%A-Z_]*Cache\.txt$';
-$tmp_garbage[] = '^oxpec\_menu\_[0-9a-z\^%A-Z_]*\_xml\.txt$';
-$tmp_garbage[] = '^oxpec\_oxuser\_[0-9a-z\^%A-Z_]*\.txt$';
-$tmp_garbage[] = '^oxpec\_oxshops\_[0-9a-z\^%A-Z_]*\.txt$';
-
-//Scan folder and delete
-
-$files = scandir($tmp_folder . '/');
-$count = 0;
-foreach ($tmp_garbage as $regex) {
-    foreach ($files as $file) {
-        if (preg_match('/' . $regex . '/', $file) && file_exists($tmp_folder . '/' . $file)) {
-            unlink($tmp_folder . '/' . $file);
-            $count++;
+    public function clearTemp()
+    {
+        $tempDir = oxRegistry::getConfig()->getConfigParam('sCompileDir');
+        $rdi = new RecursiveDirectoryIterator($tempDir);
+        $rii = new RecursiveIteratorIterator($rdi);
+        foreach ($rii as $entry) {
+            if (!$entry->isFile()) {
+                continue;
+            }
+            unlink($entry->getRealPath());
         }
+        echo '&nbsp;<span style="color: #7FFF00">✔</span><span></span> <sup>' . date("H:i:s") . '</sup>';
     }
 }
-
-// Sweep Smarty-TMP
-
-//Define the Files to sweep by Regex
-$smartyfiles = scandir($tmp_folder . '/smarty/');
-$smartycount = 0;
-
-foreach ($smartyfiles as $file) {
-    if ($file != '.' && $file != '..') {
-        unlink($tmp_folder . '/smarty/' . $file);
-        $smartycount++;
-    }
-}
-
-$sOut = '<html><body><pre>' . "\n";
-$sOut .= $count . ' Temp-File(s) deleted!' . "\n";
-$sOut .= $smartycount . ' Smarty-Tempfile(s) deleted!</pre>' . "\n";
-$sOut .= '</body></html>';
-
-echo $sOut;
